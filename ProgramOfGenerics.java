138af5c0ace22148ba476da5dd19041945882140
kiran68 <kiranurshil88@gmail.com>
2023-04-04 18:53:14 +0530

    Add: Maximim Integer Number

---
+++ ProgramOfGenerics.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.generics;
+
+public class ProgramOfGenerics {
+	 public static <K extends Comparable<K>>K maximum (K x, K y, K z){
+		 
+		 K max = x;
+		 
+		 if (y.compareTo(max) > 0) {
+			 max = y;
+			 
+		 }
+		 if (z.compareTo(max)>0) {
+			 max = z;
+		 }
+		 
+		 return max;
+		 
+	 }
+	 public static void main(String[] args) {
+		System.out.println (maximum(2, 4, 5));
+	 }
+}
