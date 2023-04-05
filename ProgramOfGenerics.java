70d42875f72a54922a1b8f96e9c6c56c8e171f4a
kiran68 <kiranurshil88@gmail.com>
2023-04-05 18:54:09 +0530

    Add: Print  Generics Method

--- ProgramOfGenerics.java
+++ ProgramOfGenerics.java
@@ -25,6 +25,8 @@ public class ProgramOfGenerics {
 	 public static void main(String[] args) {
 		
 		
-		System.out.println("Maximum String Value : " + maximum('K','I','R', 'Z', 'N'));
+		System.out.println("Maximum Integer Value : " + maximum(14, 30, 47,10,9));
 	 }
-}
+	
+	}
+
